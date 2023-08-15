01c748e93c21a05684fa6a13db6857c194caa872
chain <klaus.mika1052@gmail.com>
2023-08-15 16:06:15 -0300

    Streams no Node.js - Criando middleware de JSON

---
+++ src/middlewares/json.js
@@ -0,0 +1,15 @@
+export async function json(request, response) {
+  const buffers = []
+
+  for await (const chunk of request) {
+    buffers.push(chunk)
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch (err) {
+    request.body = null
+  }
+
+  response.appendHeader('Content-Type', 'application/json')
+}
