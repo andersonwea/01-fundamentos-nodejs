1ac09092cff0dbef1a9b58c651602bb0ed867152
chain <klaus.mika1052@gmail.com>
2023-08-15 17:50:44 -0300

    Rotas da aplicação - Rotas com parâmetros (Regex)

--- src/server.js
+++ src/server.js
@@ -8,13 +8,17 @@ const server = http.createServer(async (request, response) => {
   await json(request, response)
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   })
 
   if (!route) {
     return response.writeHead(404).end()
   }
   
+  const routeParams = request.url.match(route.path)
+
+  console.log(routeParams)
+
   return route.handler(request, response)
 })
 
